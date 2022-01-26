d1bd9955d9b0edb46cdd7313b778008ac2f9fff9
Piotr Wijas <piotrwijp@seznam.cz>
2022-01-26 19:01:55 +0100

    Changed baseline minimum stats to 1. Added powers support. Added Goliath power. Added maximum stats to attributeChanger.

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-
+    
     public float str, agi, tgh, spd, nrg, xp, lvl, luck, o_luck, o_str, o_agi, o_tgh, o_spd, o_nrg, o_xp, o_lvl, o_price, price;
-    public float minStr, minAgi, minTgh, minSpd, minNrg, minXp, minLvl, o_minStr, o_minAgi, o_minTgh, o_minSpd, o_minNrg, o_minXp, o_minLvl, o_minPrice, minPrice,minLuck,o_minLuck;
+    public float minStr=1, minAgi=1, minTgh=1, minSpd=1, minNrg=1, minXp, minLvl=1, o_minStr, o_minAgi, o_minTgh, o_minSpd, o_minNrg, o_minXp, o_minLvl, o_minPrice, minPrice,minLuck,o_minLuck;
     public float maxLuck = 10,maxStr=10, maxAgi=10, maxTgh=10, maxSpd=10, maxNrg=10, maxXp=10, maxLvl=10, o_maxStr=10, o_maxAgi=10, o_maxTgh=10, o_maxSpd=10, o_maxNrg=10, o_maxXp=1000000000000000f, o_maxLvl=9999, o_maxPrice=10000000000f, maxPrice= 10000000000f;
     public string specials, o_specials;
     public string owner, o_owner;
@@ -107,25 +107,79 @@ public class Character : MonoBehaviour
                         price = price + amount;
                     }
                     break;
-
+                case "maxluck":
+                case "maxLuck":
+                case "max luck":
+                    maxLuck = maxLuck + amount;
+                    break;
+                case "maxstr":
+                case "maxStr":
+                case "max str":
+                    maxStr = maxStr + amount;
+                    break;
+                case "maxagi":
+                case "maxAgi":
+                case "max agi":
+                    maxAgi = maxAgi + amount;
+                    break;
+                case "maxtgh":
+                case "maxTgh":
+                case "max tgh":
+                    maxTgh = maxTgh + amount;
+                    break;
+                case "maxspd":
+                case "maxSpd":
+                case "max spd":
+                    maxSpd += amount;
+                    break;
+                case "maxnrg":
+                case "maxNrg":
+                case "max nrg":
+                    maxNrg += amount;
+                    break;
             }
         }
-        updatePrice();
+        updatePrice();//This should be eventually removed from this function.
     }
     public void updatePrice()
     {
         price = 0;
-        for (int i = 0; i <= str; i++)
+        price += getAttributePrice(str);
+        price += getAttributePrice(agi);
+        price += getAttributePrice(tgh);
+        price += getAttributePrice(spd);
+        price += getAttributePrice(nrg);
+        price += getAttributePrice(luck);
+        foreach (string special in this.specials.Split('#'))
+        {
+            price += getSpecialPrice(special);
+        }    
+    }
+    public float getSpecialPrice(string power)
+    {
+        switch(power)
+        {
+            case "1goliath":
+                return 1000;
+        }
+        return 0;
+    }
+    public float getAttributePrice(float attribute)
+    {
+        if (attribute == 0)
+            return -1000;
+        float price = 0;
+        for (int i = 0; i <= attribute; i++)
             price += 100 * i;
-        for (int i = 0; i <= agi; i++)
-            price += 100 * i;
-        for (int i = 0; i <= tgh; i++)
-            price +=  100 * i;
-        for (int i = 0; i <= spd; i++)
-            price += 100 * i;
-        for (int i = 0; i <= nrg; i++)
-            price +=  100 * i;
-        for (int i = 0; i <= luck; i++)
-            price += 100 * i;
+        return price;
+    }
+    public void addSpecial(string special)
+    {
+        this.specials = specials + "#" + special;
+    }
+    public void removeSpecial(string special)//From now on specials cannot contain #[number]
+    {
+        this.specials = this.specials.Replace("#"+special, "");
+        this. specials =this.specials.Replace("##", "");
     }
 }
