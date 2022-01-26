b508ef1db5204a0636db3af91ba6ad9db015367a
Janusz Wijas <janusz.wijas@volny.cz>
2022-01-26 22:20:39 +0100

    Added CoreAbilitiesComponent

---
+++ Assets/Components/CoreAbilitiesComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Ability 
+{
+    public int m_Min;
+    public int m_Max;
+    public int m_Base;
+}
+
+public class CoreAbilitiesComponent : MonoBehaviour
+{
+    public Ability m_Str;
+    public Ability m_Dex;
+    public Ability m_Con;
+    public Ability m_Wis;
+    public Ability m_Int;
+    public Ability m_Cha;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
